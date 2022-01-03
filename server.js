ca200b6b2bcc30d7216e0650c2e15212bba43c0b
Armando <arman77mx@live.com.mx>
2022-01-02 20:00:01 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('<h1>Hola Unifin 3<h1/>')
+    res.end('<h1>Hola Unifin 30<h1/>')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log('Listening'))
